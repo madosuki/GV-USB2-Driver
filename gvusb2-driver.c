dc8c7d671e40c4963a028e283a0d2f101d0462a5
madosuki <yurikasamaaikatsu@gmail.com>
2026-06-26 00:41:09 +0900

    test change

--- gvusb2-driver.c
+++ gvusb2-driver.c
@@ -19,43 +19,42 @@ static const struct usb_device_id gvusb2_id_table[] = {
 };
 MODULE_DEVICE_TABLE(usb, gvusb2_id_table);
 
-static int gvusb2_probe(struct usb_interface *intf,
-	const struct usb_device_id *id)
-{
-	int ret;
-
-	ret = gvusb2_vid_probe(intf, id);
-	if (ret != -ENODEV)
-		return ret;
-
-	return gvusb2_snd_probe(intf, id);
-}
-
-static void gvusb2_disconnect(struct usb_interface *intf)
-{
-	enum gvusb2_interface_type *type = usb_get_intfdata(intf);
-
-	if (!type)
-		return;
-
-	switch (*type) {
-	case GVUSB2_INTF_VIDEO:
-		gvusb2_vid_disconnect(intf);
-		break;
-	case GVUSB2_INTF_SOUND:
-		gvusb2_snd_disconnect(intf);
-		break;
-	default:
-		usb_set_intfdata(intf, NULL);
-		break;
-	}
-}
-
-static struct usb_driver gvusb2_usb_driver = {
-	.name = "gvusb2",
-	.probe = gvusb2_probe,
-	.disconnect = gvusb2_disconnect,
+static struct usb_driver gvusb2_video_driver = {
+	.name = "gvusb2-video",
+	.probe = gvusb2_vid_probe,
+	.disconnect = gvusb2_vid_disconnect,
 	.id_table = gvusb2_id_table,
 };
 
-module_usb_driver(gvusb2_usb_driver);
+static struct usb_driver gvusb2_audio_driver = {
+	.name = "gvusb2-audio",
+	.probe = gvusb2_snd_probe,
+	.disconnect = gvusb2_snd_disconnect,
+	.id_table = gvusb2_id_table,
+};
+
+static int __init gvusb2_module_init(void)
+{
+	int ret;
+
+	ret = usb_register(&gvusb2_video_driver);
+	if (ret)
+		return ret;
+
+	ret = usb_register(&gvusb2_audio_driver);
+	if (ret) {
+		usb_deregister(&gvusb2_video_driver);
+		return ret;
+	}
+
+	return 0;
+}
+
+static void __exit gvusb2_module_exit(void)
+{
+	usb_deregister(&gvusb2_audio_driver);
+	usb_deregister(&gvusb2_video_driver);
+}
+
+module_init(gvusb2_module_init);
+module_exit(gvusb2_module_exit);
